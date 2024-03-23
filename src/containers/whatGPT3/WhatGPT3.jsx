512242a1ecab74cd1ea0aff4b97765582f0acdc4
juan diego llorente orozco <llorentejuandiego@gmail.com>
2024-03-23 13:22:35 -0500

    Finished the header component as well as the brand component, started working on the what gpt3 component

--- src/containers/whatGPT3/WhatGPT3.jsx
+++ src/containers/whatGPT3/WhatGPT3.jsx
@@ -1,9 +1,14 @@
 import React from 'react'
 import "./whatGPT3.css"
+import { Feature } from '../../components'
 
 function WhatGPT3() {
   return (
-    <div>WhatGPT3</div>
+    <div className='gpt3__whatgpt3 section__margin' id="whpt3">
+      <div className='gpt3__whatgpt3-feature'>
+        <Feature />
+      </div>
+    </div>
   )
 }
 
