d2e27711a70a80c4933df97893021841fa4afb4d
juan diego llorente orozco <llorentejuandiego@gmail.com>
2024-03-22 23:57:16 -0500

    Just finished the navbar component and started the header component I am about to finish but I still need to work on the img

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -26,7 +26,7 @@ function Navbar() {
         <div className='gpt3__navbar-links_container'>
           <Menu />
         </div>
-      </div>
+      </div>          
       <div className='gpt3__navbar-sign'>
         <p>Sign in</p>
         <button type='button'>Sign up</button>
@@ -36,18 +36,21 @@ function Navbar() {
         ?<RiCloseLine color ="#fff" size={27} onClick={() => setToggleMenu(false)} />
         :<RiMenu3Line color ="#fff" size={27} onClick={() =>setToggleMenu(true)}/>
         } 
-        {toggleMenu && (
-          <div className='gpt3_navbar-menu_container scale-up-center'>
+        {toggleMenu && (  
+          <div className='gpt3__navbar-menu_container scale-up-center'>
             <div className='gpt3__navbar-menu_container-links'>
-              <Menu />
-              <div className='gpt3__navbar-menu_container-links-sign'>
+              <p><a href="#home">Home</a></p>
+              <p><a href="#wgpt3">What is GPT3?</a></p>
+              <p><a href="#possibility">Open AI</a></p>
+              <p><a href="#features">Case Studies</a></p>
+              <p><a href="#blog">Library</a></p>
+            </div>
+            <div className='gpt3__navbar-menu_container-links-sign'>
               <p>Sign in</p>
               <button type='button'>Sign up</button>
-      </div>
             </div>
           </div>
-        )
-        }
+        )}
       </div>
     </div>
   )
--- src/containers/header/Header.jsx
+++ src/containers/header/Header.jsx
@@ -1,9 +1,29 @@
 import React from 'react'
 import "./header.css"
+import people from '../../assets/people.png';
+import ai from '../../assets/ai.png';
 
 function Header() {
   return (
-    <div>Header</div>
+    <div className='gpt3__header section__padding' id='home'>
+      <div className='gpt3__header-content'>
+        <h1 className='gradient__text'>Let's Build Something amazing with GPT-3 OpenAI</h1>
+        <p>Generative Pre-trained Transformer 3, is an advanced language model developed by OpenAI. With 175 billion parameters, it's one of the largest models ever created, capable of generating coherent and contextually relevant text given a prompt. 
+        </p>
+        <div className="gpt3__header-content__input">
+          <input type="email" placeholder='Please input your email address' />
+          <button type='button'>Get Started</button>
+        </div>
+        <div className="gpt3__header-content__people">
+          <img src={people} alt="people"/>
+          <p>1,600 people requested access a visit in last 24 hours</p>
+        </div>
+        <div className='gpt3__header-image'>
+          <img src={ai} alt="ai"/>
+        </div>
+      </div>
+     
+    </div>
   )
 }
 
